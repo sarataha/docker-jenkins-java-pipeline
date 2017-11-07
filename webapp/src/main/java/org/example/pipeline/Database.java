c1a83de5e437c5f2266e4e6120f72bee834293a6
ElZayat <st.elzayat@gmail.com>
2017-11-07 13:35:45 +0200

    connect to db, update jenkins file, add docker-compose file

---
+++ webapp/src/main/java/org/example/pipeline/Database.java
@@ -0,0 +1,34 @@
+package org.example.pipeline;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.CouchbaseCluster;
+
+/**
+ * @author arungupta
+ */
+public class Database {
+
+    static CouchbaseCluster cluster;
+    static Bucket bucket;
+
+    public static final CouchbaseCluster getCluster() {
+        if (null == cluster) {
+            System.out.println(System.getenv());
+            String host = System.getenv("DB_URI");
+            if (null == host) {
+                System.err.println("Invalid host: " + host);
+                System.exit(1);
+            }
+            System.out.println("Using host: " + host);
+            cluster = CouchbaseCluster.create(host);
+        }
+        return cluster;
+    }
+
+    public static Bucket getBucket(String bucketName) {
+        if (null == bucket) {
+            bucket = getCluster().openBucket(bucketName);
+        }
+        return bucket;
+    }
+}
